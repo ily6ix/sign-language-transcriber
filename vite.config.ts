97ace13cf741aaf9250cef48714c4c1775e85343
Goitseone Rakgomo <119797578+ily6ix@users.noreply.github.com>
2025-09-04 09:34:34 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
-  base: "./" // ✅ ensures it loads correctly on Vercel
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
